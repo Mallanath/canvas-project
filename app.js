4a2f20cb6bdeeb06de31beef7e41947383a2043a
Mallanath <mallanathmise443@gmail.com>
2024-01-18 08:31:55 +0530

    step 4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const canvas = document.querySelector('#draw')
 const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
@@ -11,7 +12,23 @@ let lastX = 0
 let lastY = 0
 
 function draw(e) {
-	console.log(e)
+  if (!isDrawing) return // stop the fn from running when they are not moused down
+  console.log(e)
+  ctx.beginPath()
+  ctx.moveTo(lastX, lastY)
+  ctx.lineTo(e.offsetX, e.offsetY)
+  ctx.stroke();
+
+  [lastX, lastY] = [e.offsetX, e.offsetY]
+  
 }
 
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY]
+})
+
+
 canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mouseup', () => isDrawing = false)
+canvas.addEventListener('mouseout', () => isDrawing = false)
